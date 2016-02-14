3ef5c1367440ae53e63ffd821246eb5418fe47c5
dreamingblackcat <dreamingblackcat10@gmail.com>
2016-02-14 11:29:36 +0630

    Initial Commit with DB CRUD working

---
+++ lib/db.rb
@@ -0,0 +1,58 @@
+class DB
+  def initialize(table_name, store)
+    @store             = store
+    @table_name        = table_name
+    @_id               = 0
+    @store.transaction do
+      @store[@table_name] = Hash.new
+    end    
+  end
+
+  def << item
+    @_id += 1
+    item.id = @_id
+    @store.transaction do
+      @store[@table_name][@_id.to_s] = item 
+    end
+  end
+
+  def all
+    items = []
+    @store.transaction do
+      @store[@table_name].each do|key, value|
+        items << value
+      end
+    end
+    items
+  end
+
+  def [] id
+    item = nil
+    @store.transaction do
+      item = @store[@table_name][id.to_s]
+    end
+    item
+  end
+
+  def update id, update_params
+    item = nil
+    @store.transaction do
+      item = @store[@table_name][id.to_s]
+      @store.abort unless item
+      update_params.each do|key, value|
+        item[key] = value
+      end
+      @store[@table_name][id.to_s] = item
+    end
+    item
+  end
+
+  def delete id
+    @store.transaction do
+      table = @store[@table_name]
+      table.delete(id.to_s)
+      @store[@table_name] = table
+    end
+  end
+  
+end
---
+++ test/db_test.rb
@@ -0,0 +1,41 @@
+require 'minitest/autorun'
+require 'pstore'
+require_relative '../lib/db'
+
+describe DB do
+  PSTORE = PStore.new "../db/test_db.pstore"
+  Book    = Struct.new(:title, :author, :id)
+  before do
+    @db     = DB.new 'books', PSTORE
+  end
+
+  it 'allows to add a new book' do
+    book = Book.new("my title", "my author")
+    @db<< book
+    book.id = 1
+    PSTORE.transaction(true) { PSTORE['books']["1"].must_equal book }
+  end
+  it 'allows to get all books' do
+    book = Book.new("my title", "my author", 1)
+    PSTORE.transaction { PSTORE['books'] = {"1" => book} }
+    @db.all.must_equal [book]
+  end
+  it 'allows to get a book by id' do
+    book = Book.new("my title", "my author", 1)
+    PSTORE.transaction { PSTORE['books'] = {"1" => book} }
+    @db["1"].must_equal book
+  end
+  it 'allows to update a book by id' do
+    book = Book.new("my title", "my author", 1)
+    PSTORE.transaction { PSTORE['books'] = {"1" => book} }
+    @db.update "1", {title: "updated title"}
+    PSTORE.transaction { PSTORE['books']["1"].title.must_equal "updated title"}
+  end
+  it 'allows to delete a book by id' do
+    book = Book.new("my title", "my author", 1)
+    PSTORE.transaction { PSTORE['books'] = {"1" => book} }
+    @db.delete "1"
+    PSTORE.transaction { PSTORE['books']["1"].must_be_nil }
+  end
+end
+
